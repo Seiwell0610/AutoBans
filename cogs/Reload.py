a67edf59aa7a4bbdc54d77abaf3f891bfba2ee91
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2021-01-03 12:41:54 +0900

    Update Reload.py

--- cogs/Reload.py
+++ cogs/Reload.py
@@ -4,12 +4,16 @@ from discord.ext import commands
 
 print(f"{os.path.basename(__file__)}の読み込み完了")
 
+admin_list = [343956207754805251]
+
 class System(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def reload(self, ctx):
+        if not admin_list in ctx.author.id:
+            return await ctx.send("管理者専用コマンドです。")
         msg = await ctx.send("更新中")
         for cog in os.listdir("./cogs"):
             if cog == 'Reload.py':
